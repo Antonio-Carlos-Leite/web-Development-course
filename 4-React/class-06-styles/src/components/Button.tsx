ddbf87a50a0d2c788743bada86ed8c48dcc5c50c
antonio carlos <antoniocarlosleite1992@gmail.com>
2024-03-11 16:24:35 -0300

    add class 06 styles

---
+++ 4-React/class-06-styles/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonContainer } from "./style";
+
+export function Button() {
+  return (
+    <>
+      <ButtonContainer>Click</ButtonContainer>
+    </>
+  );
+}
+
+
---
+++ 4-React/class-06-styles/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/Button";
+
+export function App() {
+  return (
+    <>
+      <h1>Class 06 Styles</h1>
+      <Button/>
+    </>
+  );
+}
+
+
